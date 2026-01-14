3d8dee93287afd8698c38962fbe9cd721645201f
manu-hn <manuu.hn11@gmail.com>
2026-01-14 13:29:55 +0530

    `Refactor ESGJourneyCard component to add line clamping and toggle view more functionality`

--- src/component/about-us/ESGJourneyCard.jsx
+++ src/component/about-us/ESGJourneyCard.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
 
-
-
 const ESGJourneyCard = ({ item }) => {
     const [viewMore, setViewMore] = useState(false);
 
     return (
         <div className='border-gray-400 border-[0.015em] shadow-lg mx-[3%] bg-white dark:bg-slate-600 p-[3%] rounded-md lg:min-h-[15vw] hover:shadow-2xl hover:scale-[0.95] duration-75'>
             <div>
-                <h3 className='text-[1em] font-bold mb-[2%]'>{item?.title}</h3>
-                <p className='text-[0.95em]'> 
-                    <span>{item?.description?.substring(0, viewMore ? item?.description?.length : 221)}</span>
-                    <span onClick={() => setViewMore(!viewMore)} className='text-[0.85em] text-[#007BFF] cursor-pointer mx-[1.5%]'>{viewMore ? "View Less" : "View More"}</span>
+                <h3 className={`text-[1em] font-bold mb-[2%] ${viewMore ? "" : "line-clamp-1"}`}>{item?.title}</h3>
+                <p className={`text-[0.95em] ${viewMore ? "" : "line-clamp-3"}`}> 
+                    <span>{item?.description?.substring(0, viewMore ? item?.description?.length : 200)}</span>
                 </p>
+                <span onClick={() => setViewMore(!viewMore)} className='text-[0.85em] text-[#007BFF] cursor-pointer mx-[1.5%]'>{viewMore ? "View Less" : "View More"}</span>
+
             </div>
             <div className='mt-[1.5%] flex justify-between'>
                 <div className='flex items-center w-[80%]'>
                     <img src={item?.profile} alt="" className='h-16 w-16 rounded-full' />
                     <p className='text-[0.85em] flex flex-col mx-[2%]'>
                         <span>{item?.name}</span>
-                        <span>{item?.designation}</span>
+                        <span className={` ${viewMore ? "" : "line-clamp-2"}`}>{item?.designation}</span>
                     </p>
                 </div>
                 {/* <img src={item?.profile} alt="" className='w-28' /> */}
